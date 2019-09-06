f34d4519d42f6a5dbc709724daa5e273b745e44f
Dane Johnson <dane@wustl.edu>
2019-09-06 13:40:17 -0500

    View page for text files

--- view.php
+++ view.php
@@ -1,7 +1,21 @@
 <?php
 require "./guard.php";
+$file = "/var/simplefileserver/$user/" . htmlspecialchars($_GET['name']);
+switch(mime_content_type($file)) {
+  case "text/plain":
+    $filetype = "text";
+    break;
+  default:
+    $filetype = "unknown";
+}
 ?>
 <!DOCTYPE html>
 <html>
   <?php require "./header.php" ?>
+  <?php if ($filetype == "text") { ?>
+    <pre><?php echo file_get_contents($file) ?></pre>
+  <?php } else { ?>
+    <p>Sorry, we cannot open this type of file</p>
+  <?php }
+  ?>
 </html>
